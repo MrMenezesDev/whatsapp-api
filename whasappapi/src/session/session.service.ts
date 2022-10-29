d747a3e6429a232e92858af9b96eb861d9e65a28
Erick Menezes <erick.menezes@cubos.io>
2022-10-29 19:19:54 -0300

    fix: fixed session save data

--- whasappapi/src/session/session.service.ts
+++ whasappapi/src/session/session.service.ts
@@ -6,16 +6,13 @@ import { existsSync, readFileSync, writeFileSync } from "fs";
 export interface ISession {
     id: string;
     socketId: string;
-    description: string;
     ready: boolean;
     client: any;
 }
 
 @Injectable()
 export class SessionService {
-    private sessions: {
-        [id: string]: ISession;
-    }
+    private sessions: ISession[];
     private sessionFile: string;
 
     constructor(
@@ -45,7 +42,7 @@ export class SessionService {
     }
 
     getSession(id: string): ISession {
-        const session = this.sessions[id];
+        const session = this.sessions.find(sess => sess.id = id)
         if (session) {
             return session;
         } else {
@@ -54,36 +51,17 @@ export class SessionService {
     }
 
     updateSession(id: string, session: Partial<ISession>): ISession {
+        const sessionIndex = this.sessions.findIndex(sess => sess.id == id);
         const oldSession = this.getSession(id);
         const newSession = { ...oldSession, ...session }
-        this.sessions[oldSession.id] = newSession;
+        this.sessions[sessionIndex] = newSession;
         this.saveSessionsFile();
         return newSession;
     }
 
     createSession(id: string) {
-        
-        const session: ISession = {client: undefined, description: "", ready: false, id, socketId: ""};
-        session.client = new Client({
-            restartOnAuthFail: true,
-            puppeteer: {
-                headless: true,
-                args: [
-                    '--no-sandbox',
-                    '--disable-setuid-sandbox',
-                    '--disable-dev-shm-usage',
-                    '--disable-accelerated-2d-canvas',
-                    '--no-first-run',
-                    '--no-zygote',
-                    // '--single-process', // <- this one doesn't works in Windows
-                    '--disable-gpu'
-                ],
-            },
-            authStrategy: new LocalAuth({
-                clientId: id
-            })
-        });
-        this.sessions[id] = session;
+        const session: ISession = {client: undefined, ready: false, id, socketId: ""};
+        this.sessions.push(session);
         this.saveSessionsFile();
         return session;
     }
