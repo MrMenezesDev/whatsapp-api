ff649eb866c80c32bbbc31bf10958576671a2232
Erick Menezes <erick.menezes@cubos.io>
2022-10-29 16:21:45 -0300

    chore: delete dist

--- whasappapi/dist/user/user.service.d.ts
+++
@@ -1,9 +0,0 @@
-export interface IUser {
-    id: string;
-    name: string;
-}
-export declare class UserService {
-    users: IUser[];
-    getById(userId: string): IUser;
-    getByUserName(userName: string): IUser;
-}
--- whasappapi/dist/whasapp/whasapp.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { ISession } from 'src/session/session.service';
-export declare class WhasappService {
-    constructor();
-    sendMessage(number: string, message: string, session: ISession): Promise<any>;
-    listMessage(number: string, session: ISession): Promise<any[]>;
-    private phoneNumberFormatter;
-}
